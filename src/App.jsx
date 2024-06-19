04e59363c334f72a707ba12d7e698d349d71ed0d
J-Saisaran <saransai027@gmail.com>
2024-06-19 11:07:50 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import "./App.css";
+import { useState } from 'react';
+const App = () => {
+  
+const person=[
+  {name:"Ajay", age:"20", img:"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"},
+  {name:"Sai" , age:"20" ,img:"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"},
+  {name:"Sai"  ,age:"20" ,img:"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg"}
+]
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <div className='App'>
+
+   
+   {person.map((usr)=>(
+    <Welcome1 name = {usr.name} age={usr.age} img={usr.img}/>
+   ))}
+   
+    </div>
   )
 }
 
+// function Welcome(props){
+//   console.log(props)
+// return(
+//   <h1>Hi {props.name} </h1>  
+// )
+// }
+
+function Welcome1(props){
+  console.log(props)
+return(
+  <>
+  <div>
+  <img className="Photo" src={props.img} />
+  <h1>{props.name}</h1>
+  <Counter/>
+  </div>
+  </>
+  
+)
+}
+
+function Counter(){
+  //let like= 10;
+  const [ like , setLike]=useState(0);
+  const [ dislike , setDislike]=useState(0);
+  console.log(like);
+  console.log(dislike);
+  return(
+  <>
+
+    <button onClick = {() => {
+     setLike(like+1);
+
+    }}>👍{like}</button>
+
+  <button onClick = {() => {
+     setDislike(dislike+1);
+    }}>👎{dislike}</button>
+    
+  </>)
+}
+
+
+// function Sample({lk}){
+
+//   return(
+//     <>
+//      <div>Like value is {lk}</div>
+//     </>
+//   )
+// }
+// function Sample1({dlk}){
+
+//   return(
+//     <>
+//      <div>Dislike value is {dlk}</div>
+//     </>
+//   )
+// }
 export default App
